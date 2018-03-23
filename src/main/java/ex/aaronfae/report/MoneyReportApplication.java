8bf5f1dc18e1669411ae749b898923a13b104b92
AaronFae <AaronFae@163.com>
2018-03-23 20:50:33 +0800

    first commit

---
+++ src/main/java/ex/aaronfae/report/MoneyReportApplication.java
@@ -0,0 +1,12 @@
+package ex.aaronfae.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyReportApplication.class, args);
+	}
+}
---
+++ src/test/java/ex/aaronfae/report/MoneyReportApplicationTests.java
@@ -0,0 +1,16 @@
+package ex.aaronfae.report;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneyReportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
